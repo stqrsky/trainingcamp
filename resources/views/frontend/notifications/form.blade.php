77a063a38c2ef16690fb3f7d763dbea04f5cf9f9
stqr <starsky.m@live.com>
2020-05-08 15:56:43 +0200

    add form.blade and its route

---
+++ resources/views/frontend/notifications/form.blade.php
@@ -0,0 +1,12 @@
+<div class="form-group row">
+    <label for="name" class="col-sm-12 col-form-label">Title</label>
+    <div class="col-sm-12">
+        <input type="text" class="form-control" id="name" placeholder="Title" name="title">
+    </div>
+</div>
+<div class="form-group row">
+    <label for="note" class="col-sm-12 col-form-label">Note</label>
+    <div class="col-sm-12">
+        <textarea class="form-control" name="note" id="note" rows="8" cols="80"></textarea>
+    </div>
+</div>
