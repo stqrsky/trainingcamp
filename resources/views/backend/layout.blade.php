e669b4aba38f3982e556a96ee9d90bcd6f6c6b39
stqr <starsky.m@live.com>
2020-05-12 09:59:43 +0200

    move style out of blade, into scss

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -7,27 +7,9 @@
     <title>{{ isset($title) ? $title : 'Training Camp' }}</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/floating-labels/">
-    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <meta name="theme-color" content="#563d7c">
 
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="{{ asset('/css/signin.css') }}">
 </head>
