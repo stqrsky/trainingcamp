f061b1ee0f878f57bdfdd2f27c271da16921e94c
stqr <starsky.m@live.com>
2020-05-13 14:05:14 +0200

    UserSeed made my nose bleed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,7 +13,14 @@ class UserSeeder extends Seeder
     {
         //creating User using factory, then persist them to database, with 15 Users (second arg of factory)
 
-
+        factory(App\Models\User::class, 15)->create()->each(function($user) {
+            $user->roles()->attach(rand(1, 3));
+            $skill = rand(1, 5);
+            while ($skill < 5) {
+                $user->skills()->attach($skill);
+                $skill += 1;
+            }
+        })
 
         //try to pluck it out of the database 
     }
