c064934f1efed6ac2389194e49d10b85040adf54
stqr <starsky.m@live.com>
2020-05-21 09:20:01 +0200

    accessor and mutator

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Schedule extends Model
 {
@@ -16,5 +17,27 @@ class Schedule extends Model
             \App\Models\User::class,
             'schedule_participant'
         );
+    }
+
+    public function getStartAttribute($start)
+    {
+        return Carbon::parse($start)->format('H:i');
+    }
+
+    public function getEndAttribute($end)
+    {
+        return Carbon::parse($end)->format('H:i');
+    }
+
+    public function getDateFormatAttribute()
+    {
+        return Carbon::parse($this->date)->format('d/m/Y');
+    }
+
+    public function getStartEndTimeAttribute()
+    {
+        $start = Carbon::parse($this->start)->format('h:i a');
+        $end = Carbon::parse($this->end)->format('h:i a');
+        return "$start - $end";
     }
 }
