25b2ddb26e060693053afe5894bd7e186debf6e2
stqr <starsky.m@live.com>
2020-05-21 08:27:54 +0200

    Userprofile and setting

--- resources/views/frontend/users/accountsetting.blade.php
+++ resources/views/frontend/users/accountsetting.blade.php
@@ -10,7 +10,8 @@
                 <div class="form-group row">
                     <label for="email" class="col-sm-12 col-form-label">Email</label>
                     <div class="col-sm-12">
-                        <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                        <input type="email" class="@error('email') is-invalid @enderror form-control" id="email" placeholder="Email" name="email" value="{{ $user->email }}">
+                        @error('email')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
                     </div>
                 </div>
                 <div class="form-group row">
@@ -25,19 +26,22 @@
                 <div class="form-group row">
                     <label for="current_password" class="col-sm-12 col-form-label">Current Password</label>
                     <div class="col-sm-12">
-                        <input type="password" class="form-control" id="current_password" placeholder="Current Pasword" name="current_password">
+                        <input type="password" class="@error('current_password') is-invalid @enderror form-control" id="current_password" placeholder="Current Pasword" name="current_password" value="{{ old('current_password') }}">
+                        @error('current_password')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="new_password" class="col-sm-12 col-form-label">New Password</label>
                     <div class="col-sm-12">
-                        <input type="password" class="form-control" id="new_password" placeholder="New Password" name="new_password">
+                        <input type="password" class="@error('new_password') is-invalid @enderror form-control" id="new_password" placeholder="New Password" name="new_password" value="{{ old('new_password') }}">
+                        @error('new_password')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="confirm_new_password" class="col-sm-12 col-form-label">Confirm New Password</label>
                     <div class="col-sm-12">
-                        <input type="password" class="form-control" id="confirm_new_password" placeholder="Height" name="confirm_new_password">
+                        <input type="password" class="@error('new_password') is-invalid @enderror form-control" id="new_password_confirmation" placeholder="Height" name="new_password_confirmation" value="{{ old('new_password_confirmation') }}">
+                        @error('new_password')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
                     </div>
                 </div>
                 <div class="form-group row">
--- resources/views/frontend/users/profile.blade.php
+++ resources/views/frontend/users/profile.blade.php
@@ -6,27 +6,35 @@
         <div class="col-12 pb-3 mb-5 content">
 
             <div aria-live="assertive" aria-atomic="true">
-                <div class="toast-header d-flex align-items-center">
-                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle mr-3" height="70px" width="70px" alt="avatar">
+                <div class="toast-header me d-flex align-items-center">
+                    <img src="{{
+                        $user->userDetail && $user->userDetail->image ? 
+                        asset($user->userDetail->image->file_name) :
+                        "https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg"
+                    }}" class="rounded-circle mr-3" height="70px" width="70px" alt="avatar">
                     <div class="d-flex flex-column">
-                        <strong class="mr-auto">Full Name</strong>
-                        <strong class="mr-auto">Team</strong>
+                        <strong class="mr-auto text-dark">{{ $user->full_name }}</strong>
+                        <strong class="mr-auto">{{ $user->team ? $user->team->name : '' }}</strong>
                     </div>
                 </div>
-                <div class="toast-body">
+                <div class="toast-body about">
                     <h4 class="card-title font-weight-bold mb-2 andreas">Coach</h4>
-                    See? Just like this.
+                    {!! $user->userDetail ? $user->userDetail->about : '' !!}
                 </div>
-                <div class="toast-body">
-                    @for($skill = 1; $skill < 10; $skill++) <span class="badge badge-secondary">Skill {{$skill}}</span>
-                        @endfor
+                <div class="toast-body skill">
+                    @foreach($user->skills as $skill)
+                    <span class="badge badge-warning">{{ $skill->name }}</span>
+                    @endforeach
                 </div>
                 <div class="toast-body d-flex justify-content-between">
-                    <a href="{{ route('user.profile.setting') }}" type="button" class="btn profilebtn1 btn-outline-dark">Profile Settings</a>
-                    <a href="{{ route('user.account.setting') }}" type="button" class="btn profilebtn2 btn-outline-dark">Account Settings</a>
+                    <a href="{{ route('user.profile.setting') }}" type="button" class="btn profilebtn btn-outline-dark">Profile Settings</a>
+                    <a href="{{ route('user.account.setting') }}" type="button" class="btn profilebtn btn-outline-dark">Account Settings</a>
                 </div>
-                <div class="toast-body d-flex justify-content-center">
-                    <a type="button" class="btn bg-danger text-white glyphicon glyphicon-log-out">Sign Out</a>
+                <div class="toast-body sign-out d-flex justify-content-center">
+                    <form action="{{ route('user.logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn bg-danger text-white glyphicon glyphicon-log-out">Sign Out</button>
+                    </form>
                 </div>
             </div>
 
