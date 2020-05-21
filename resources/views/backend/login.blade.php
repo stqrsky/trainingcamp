e7d3b5aa716b921390b704f2a110ecfea37f196a
stqr <starsky.m@live.com>
2020-05-21 08:26:51 +0200

    Login and signup part

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -1,31 +1,31 @@
 @extends('backend.layout')
 
 @section('content')
-<form class="form-signin text-center">
+<form class="form-signin text-center" method="POST" action="{{ route('login.post') }}">
     <div class="text-center">
-        <img class="img" src="{{ asset('assets/images/TCtransp.png') }}" alt="Training Camp" width="280" height="280">
+        <img class="img-login" src="{{ asset('assets/images/TCtransp.png') }}" alt="Training Camp" width="260" height="260">
     </div>
-
-    <h1 class="h3 mb-3 font-weight-normal"></h1>
+    @csrf
+    <h1 class="h3 mb-4 font-weight-normal"></h1>
     @error('error')
     <div class="alert alert-danger" role="alert">
         {{ $message }}
     </div>
     @enderror
     <div class="form-label-group">
-        <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" name="email" id="email" class="form-control" placeholder="" required autofocus>
         <label for="email">Email address</label>
         @error('email')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
     </div>
 
     <div class="form-label-group">
-        <input type="password" id="password" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="password" class="form-control" placeholder="" required>
         <label for="password">Password</label>
         @error('password')<div class="invalid-feedback float-left">{{ $message }}</div>@enderror
     </div>
 
-    <button class="btn btn-lg text-white bg-dark btn-block" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">
+    <button class="btn mt-4 btn-lg text-white btn-block" type="submit">LOGIN</button>
+    <p class="mt-3 mb-3 text-white">
         Don't have an account?
         <a class="mt-5 mb-3" href="{{ route('signup') }}">SIGN UP</a>
     </p>
--- resources/views/frontend/users/createprofile.blade.php
+++ resources/views/frontend/users/createprofile.blade.php
@@ -1,11 +1,21 @@
 @extends('frontend.layouts.app')
 
+@section('style')
+<link rel="stylesheet" href="{{ asset('select2/css/select2.min.css') }}">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-12 pb-3 content">
             <div class="card">
                 <div class="card-body">
+                    @error('error')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                    @enderror
                     <form method="POST" action="{{ route('user.setting.post') }}">
                         @csrf
                         @include('frontend.users.formprofile')
@@ -16,4 +26,26 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="{{ asset('select2/js/select2.min.js') }}"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#skills').select2({
+            placeholder: 'Select Your Skills'
+        });
+        $('#date_of_birth').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true,
+            minYear: 1901,
+            maxYear: parseInt(moment().format('YYYY'), 10),
+            locale: {
+                format: 'DD/MM/YYYY'
+            }
+        });
+    })
+</script>
 @endsection
